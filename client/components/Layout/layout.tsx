9fff7de44ba40a43e7e644cfc9ad67fdf078e365
Jurvis Tan <jurvis.tan@me.com>
2017-07-10 20:43:38 +0800

    add skeleton crew components

---
+++ client/components/Layout/layout.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+export interface LayoutProps {
+  children?: React.ReactNode;
+}
+
+export class Layout extends React.Component<LayoutProps, {}> {
+  public render() {
+    return (
+      <div className='container'>
+        <div>
+          { this.props.children }
+        </div>
+      </div>
+    )
+  }
+}
