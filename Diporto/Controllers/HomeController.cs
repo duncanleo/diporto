c17e1eca1b71c4db71314080475060c29c18b2a1
Duncan Leo <duncanleo97@gmail.com>
2017-07-11 17:39:20 +0800

    replace instances of WebApplicationBasic with Diporto

--- Diporto/Controllers/HomeController.cs
+++ Diporto/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApplicationBasic.Controllers
+namespace Diporto.Controllers
 {
     public class HomeController : Controller
     {
