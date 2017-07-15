564d8f4c06cc964ad4ee57c75135af5dece2aeae
Duncan Leo <duncanleo97@gmail.com>
2017-07-15 22:28:40 +0800

    add name field to User

---
+++ Diporto/Migrations/20170715142817_AddNameFieldToUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Diporto.Migrations
+{
+    public partial class AddNameFieldToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "user",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "user");
+        }
+    }
+}
--- Diporto/Models/User.cs
+++ Diporto/Models/User.cs
@@ -6,6 +6,9 @@ namespace Diporto.Models {
   [Table("user")]
   public class User : IdentityUser<int> {
     [Required]
+    [Column("name")]
+    public string Name { get; set; }
+    [Required]
     [Column("is_admin")]
     public bool IsAdmin { get; set; }
   }
