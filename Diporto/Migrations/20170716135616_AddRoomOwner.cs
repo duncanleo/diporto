a5c3018f484e9b260c462c4c0b7c25e3b0725e30
Duncan Leo <duncanleo97@gmail.com>
2017-07-16 21:57:23 +0800

    add fields and migration for Room owner to futureproof

---
+++ Diporto/Migrations/20170716135616_AddRoomOwner.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Diporto.Migrations
+{
+    public partial class AddRoomOwner : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "room",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "owner_id",
+                table: "room",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_room_owner_id",
+                table: "room",
+                column: "owner_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_room_user_owner_id",
+                table: "room",
+                column: "owner_id",
+                principalTable: "user",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_room_user_owner_id",
+                table: "room");
+
+            migrationBuilder.DropIndex(
+                name: "IX_room_owner_id",
+                table: "room");
+
+            migrationBuilder.DropColumn(
+                name: "owner_id",
+                table: "room");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "room",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- Diporto/Models/Room.cs
+++ Diporto/Models/Room.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Newtonsoft.Json;
 
@@ -7,11 +8,17 @@ namespace Diporto.Models {
   public class Room {
     public int Id { get; set; }
 
+    [Required]
     [Column("name")]
     public string Name { get; set; }
 
     [Column("short_code")]
     public string ShortCode { get; set; }
+
+    public User Owner { get; set; }
+    
+    [Column("owner_id")]
+    public int OwnerId { get; set; }
 
     [JsonProperty("members")]
     [NotMapped]
