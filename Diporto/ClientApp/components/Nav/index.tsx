bb8cc21edfe038ffa43fe54569247c649c9c4dac
Jurvis Tan <jurvis.tan@me.com>
2017-07-18 10:10:46 +0800

    fix nav layout with tachyons

--- Diporto/ClientApp/components/Nav/index.tsx
+++ Diporto/ClientApp/components/Nav/index.tsx
@@ -4,48 +4,29 @@ import { HTMLProps } from 'react';
 
 const styles = {
   navBar: {
-      display: "flex",
-      flexDirection: "row",
       backgroundColor: "#273CFE",
-      paddingTop: "2rem",
-      paddingBottom: "2rem"
+      padding: "2rem",
   } as HTMLProps<HTMLDivElement>,
   buttonContainer: {
       alignSelf: "center"
   } as HTMLProps<HTMLDivElement>,
-  title: {
-    color: "white",
-    margin: 0,
-    alignSelf: "center"
-  } as HTMLProps<HTMLDivElement>,
-  buttons: {
-      fontSize: "1.5rem",
-      borderRadius: "9999px",
-      paddingTop: ".5rem",
-      paddingBottom: ".5rem",
-      paddingLeft: "1rem",
-      paddingRight: "1rem",
-      color: "white",
-  },
   loginButton: {
       backgroundColor: "#50CCBC",
   },
   signUpButton: {
       color: "#FE92DE",
-      borderStyle: "solid",
-      borderWidth: ".125rem"
   }
 }
 
 export default class Nav extends React.Component<{}, {}> {
   public render() {
     return (
-      <div style={styles.navBar}>
-	  <h2 style={styles.title}>Diporto</h2>
+      <div className="flex flex-row pa3" style={styles.navBar}>
+	  <h2 className="f3 lh-title white ma0">Diporto</h2>
 	  <div style={{flexGrow: 1}}></div>
 	  <div style={styles.buttonContainer}>
-	      <a style={Object.assign({}, styles.buttons, styles.signUpButton)} href="#">Sign Up</a>
-	      <a style={Object.assign({}, styles.buttons, styles.loginButton)} href="#">Login</a>
+	      <a className="f6 link dim br3 ph3 pv2 mb2 white" style={styles.signUpButton} href="#">Sign Up</a>
+	      <a className="f6 link dim br3 ph3 pv2 mb2 white" style={styles.loginButton} href="#">Login</a>
 	  </div>
       </div>
     )
