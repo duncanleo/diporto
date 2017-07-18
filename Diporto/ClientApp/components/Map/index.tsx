38dea01698b7c73bc0fdd2fa9c24ab1fc584e70d
Jurvis Tan <jurvis.tan@me.com>
2017-07-18 19:45:24 +0800

    fixed errors

--- Diporto/ClientApp/components/Map/index.tsx
+++ Diporto/ClientApp/components/Map/index.tsx
@@ -4,7 +4,6 @@ import * as MapGL from "react-map-gl";
 export interface MapProps extends React.Props<any> {
   width: number;
   height: number;
-  viewport: MapGLViewport;
 }
 
 export interface MapComponentState {}
@@ -15,8 +14,6 @@ export default class Map extends React.Component<MapProps, MapComponentState> {
       <MapGL
 	width={this.props.width}
 	height={this.props.height}
-	latitude={this.props.viewport.latitude}
-	longitude={this.props.viewport.longitude}
       />
     )
   }
