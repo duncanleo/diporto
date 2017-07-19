5ca72fb53244818182218d8eb1dba46a417ca863
Jurvis Tan <jurvis.tan@me.com>
2017-07-19 12:29:04 +0800

    spike homepage

--- Diporto/ClientApp/components/Home.tsx
+++ Diporto/ClientApp/components/Home.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { ChangeTargetHTMLProps } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import Map from './Map';
@@ -6,15 +7,13 @@ import Map from './Map';
 export default class Home extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
 	return (
-	    <div>
-		<h1>Diporto</h1>
-		<div>
-		    <Map
-		       width={500}
-		       height={500}
-		    />
-		    <input className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns" placeholder="Search" type="text" name="search" value=""/>
-		    <input className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns" type="submit" value="subscribe"/>
+	    <div className="flex flex-column h-100 justify-center" style={{flexGrow:1}}>
+		<div className="flex flex-column items-center">
+		    <h1 className="white f1 lh-title">Diporto</h1>
+		    <div className="flex w-100 mw7">
+			<input className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-n" placeholder="Search" type="text" name="search" value=""/>
+			<input className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns" type="submit" value="search"/>
+		    </div>
 		</div>
 	    </div>
 	);
--- Diporto/ClientApp/components/Map/index.tsx
+++ Diporto/ClientApp/components/Map/index.tsx
@@ -1,20 +1,25 @@
 import * as React from "react";
-import * as MapGL from "react-map-gl";
+import MapGL from "react-map-gl";
 
 export interface MapProps extends React.Props<any> {
   width: number;
   height: number;
+  viewport: MapGLViewport;
 }
 
-export interface MapComponentState {}
-
-export default class Map extends React.Component<MapProps, MapComponentState> {
-  public render() {
+export class Map extends React.Component<MapProps, null> {
+  render() {
     return (
       <MapGL
 	width={this.props.width}
 	height={this.props.height}
+	latitude={this.props.viewport.latitude}
+	longitude={this.props.viewport.longitude}
+	zoom={this.props.viewport.zoom}
+	mapboxApiAccessToken="pk.eyJ1IjoianVydmlzIiwiYSI6ImNqNTh5MmlnNzAya3EzMXE3eGFhMWtrczQifQ.BgT8aHg52oKkki4GV8oNpA"
       />
     )
   }
 }
+
+export default Map;
