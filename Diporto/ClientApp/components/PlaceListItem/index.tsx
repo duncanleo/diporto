7cbfd4f7a214245dd81d93d668c5a1e2787c1695
Jurvis Tan <jurvis.tan@me.com>
2017-07-20 16:56:38 +0800

    remove undefined check

--- Diporto/ClientApp/components/PlaceListItem/index.tsx
+++ Diporto/ClientApp/components/PlaceListItem/index.tsx
@@ -6,19 +6,14 @@ interface PlaceListItemProps {
 
 const PlaceListItem: React.SFC<PlaceListItemProps> = (props) => {
   const { place } = props;
-
-  if (place == undefined) {
-    return <h2>Loading...</h2>
-  } else {
-    return (
-      <table>
-	<tr>{place.id}</tr>
-	<tr>{place.name}</tr>
-	<tr>{place.address}</tr>
-	<tr>{place.phone}</tr>
-      </table>
-    )
-  }
+  return (
+    <table>
+      <tr>{place.id}</tr>
+      <tr>{place.name}</tr>
+      <tr>{place.address}</tr>
+      <tr>{place.phone}</tr>
+    </table>
+  )
 }
 
 export default PlaceListItem;
