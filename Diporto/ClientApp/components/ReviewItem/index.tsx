5f05980a0a04a484e8ff52e20747b2b03f9aaea8
Jurvis Tan <jurvis.tan@me.com>
2017-07-23 14:21:01 +0800

    refine reviewitem defaults

--- Diporto/ClientApp/components/ReviewItem/index.tsx
+++ Diporto/ClientApp/components/ReviewItem/index.tsx
@@ -7,16 +7,29 @@ declare interface ReviewItemProps {
 }
 
 class ReviewItem extends React.Component<ReviewItemProps, {}> {
+  private renderUserImage() {
+    let user = this.props.review.user;
+
+    if (user !== null && user.profile_image_url !== null) {
+      return <img src={user.profile_image_url}/>
+    } else {
+      return <img className="h3 br3" src="https://s3-us-west-1.amazonaws.com/jurvis/placeholder_profile.svg"/>
+    }
+  }
+
   render() {
     const { review } = this.props;
+    const { user } = review;
 
     return (
-      <div className="flex">
+      <div className="flex mb2">
 	<div className="flex flex-column w3">
-	  <img className="h3 br3" src="https://s3-us-west-1.amazonaws.com/jurvis/placeholder_profile.svg"/>
+	  {this.renderUserImage()}
 	</div>
 	<div className="ml2 w-70">
-	  <span>{review.user == null ? "Anonymous User" : review.user.name} said: </span>
+	  <span className="fw6">
+	    {review.user == null ? "Anonymous User" : review.user.name} said:
+	  </span>
 	  <StarRating
 	    size={20}
 	    edit={false}
