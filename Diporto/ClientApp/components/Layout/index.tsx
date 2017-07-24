efb3b3da5f71c896640e29ce13360a863d137cf8
Jurvis Tan <jurvis.tan@me.com>
2017-07-24 20:40:25 +0800

    spike login

--- Diporto/ClientApp/components/Layout/index.tsx
+++ Diporto/ClientApp/components/Layout/index.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
 import { HTMLProps } from 'react';
 import Nav from '../Nav';
+import { connect } from 'react-redux';
+import { unloadedState, actionCreators } from '../../store/Auth';
 
-export class Layout extends React.Component<{}, {}> {
+export default class Layout extends React.Component<{}, {}> {
     public render() {
 	return (
 	    <div className="h-100 flex flex-column">
-		<Nav />
-		<div className="flex flex-column" style={{flexGrow: 1}}>
-		    {this.props.children}
-		</div>
+			<Nav
+				{...unloadedState}
+				{...actionCreators}
+			/>
+			<div className="flex flex-column" style={{flexGrow: 1}}>
+				{this.props.children}
+			</div>
 	    </div>
 	)
     }
