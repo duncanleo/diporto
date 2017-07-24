43a6c4f2dd60760c3b180e1fcf29c80cb8fff292
Jurvis Tan <jurvis.tan@me.com>
2017-07-24 21:38:49 +0800

    make history object common

--- Diporto/ClientApp/boot-client.tsx
+++ Diporto/ClientApp/boot-client.tsx
@@ -4,14 +4,12 @@ import * as ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
-import { createBrowserHistory } from 'history';
+import history from './history';
 import configureStore from './configureStore';
 import { ApplicationState }  from './store';
 import * as RoutesModule from './routes';
 let routes = RoutesModule.routes;
 
-// Create browser history to use in the Redux store
-const history = createBrowserHistory();
 
 // Get the application-wide store instance, prepopulating with state from the server where available.
 const initialState = (window as any).initialReduxState as ApplicationState;
