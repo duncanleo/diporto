2a17a471bd11f74483f8df5e5be987c80e33d5ce
Denise Kam <Kyw_denise@hotmail.com>
2017-07-25 22:40:06 +0800

    Added basic home page

--- Diporto/Controllers/AdminController.cs
+++ Diporto/Controllers/AdminController.cs
@@ -1,8 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Diporto.Database;
+using Diporto.Models;
+using Diporto.ViewModels.Admin;
+using System.Threading.Tasks;
 
 namespace Diporto.Controllers {
   [Route("admin")]
   public class AdminController : Controller {
+    private readonly DatabaseContext context;
+    private UserManager<User> userManager;
+    public AdminController(DatabaseContext context, UserManager<User> userManager) {
+      this.context = context;
+      this.userManager = userManager;
+    }
+
     [HttpGet]
     public ViewResult Index() {
       return View();
@@ -18,6 +30,15 @@ namespace Diporto.Controllers {
     [Route("users")]
     public ViewResult Users() {
       return View();
+    }
+
+    [HttpGet]
+    [Route("home")]
+    public async Task<ViewResult> Home() {
+      var user = await userManager.GetUserAsync(User);
+      return View(new HomeViewModel {
+        Name = user.Name
+      });
     }
   }
 }
