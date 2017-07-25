2736d719461a7ef2e63b4d4bcda4984fc7e6c7d8
Jurvis Tan <jurvis.tan@me.com>
2017-07-25 16:12:11 +0800

    spike sign up page

--- Diporto/ClientApp/routes.tsx
+++ Diporto/ClientApp/routes.tsx
@@ -5,12 +5,14 @@ import Home from './components/Home';
 import PlacesSearch from './components/PlacesSearch';
 import PlaceDisplay from './components/PlaceDisplay';
 import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 export const routes = (
     <Layout>
-			<Route exact path='/' component={ Home } />
-			<Route path="/login" component={ Login }/>
-			<Route path='/search/:searchTerm?' component={ PlacesSearch } />
-			<Route path='/place/:id' component={ PlaceDisplay } />
+		<Route exact path='/' component={ Home } />
+		<Route path="/login" component={ Login }/>
+		<Route path="/signup" component={ SignUp }/>
+		<Route path='/search/:searchTerm?' component={ PlacesSearch } />
+		<Route path='/place/:id' component={ PlaceDisplay } />
     </Layout>
 );
