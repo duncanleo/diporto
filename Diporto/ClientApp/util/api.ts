57005b482294178ab79ad3085b53616c7db231c0
Jurvis Tan <jurvis.tan@me.com>
2017-07-26 15:14:05 +0800

    write signup api endpoint

--- Diporto/ClientApp/util/api.ts
+++ Diporto/ClientApp/util/api.ts
@@ -35,6 +35,32 @@ export class DiportoApi {
 
     return fetchTask;
   }
+  signUp(regCreds: UserRegistration) {
+    const fetchTask = fetch('/api/register', {
+      method: 'POST',
+      headers: {
+	'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+	UserName: regCreds.UserName,
+	Password: regCreds.Password,
+	ConfirmPassword: regCreds.ConfirmPassword,
+	Email: regCreds.Email,
+	Name: regCreds.Name
+      })
+    })
+    .then(handleErrors)
+
+    return fetchTask;
+  }
+
+}
+
+function handleErrors(response) {
+    if (!response.ok) {
+	throw Error(response.statusText);
+    }
+    return response;
 }
 
 export default new DiportoApi();
