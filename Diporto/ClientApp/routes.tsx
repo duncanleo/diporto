35e07a5421f6468d3c8497f5954ebc31e4be3b80
Jurvis Tan <jurvis.tan@me.com>
2017-07-27 11:55:59 +0800

    add Profile page

--- Diporto/ClientApp/routes.tsx
+++ Diporto/ClientApp/routes.tsx
@@ -6,13 +6,15 @@ import PlacesSearch from './components/PlacesSearch';
 import PlaceDisplay from './components/PlaceDisplay';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import Profile from './components/Profile';
 
 export const routes = (
     <Layout>
-		<Route exact path='/' component={ Home } />
-		<Route path="/login" component={ Login }/>
-		<Route path="/signup" component={ SignUp }/>
-		<Route path='/search/:searchTerm?' component={ PlacesSearch } />
-		<Route path='/place/:id' component={ PlaceDisplay } />
+				<Route exact path='/' component={ Home } />
+				<Route path="/login" component={ Login }/>
+				<Route path="/signup" component={ SignUp }/>
+				<Route path='/search/:searchTerm?' component={ PlacesSearch } />
+				<Route path='/place/:id' component={ PlaceDisplay } />
+				<Route path='/profile/:id' component={ Profile }/>
     </Layout>
 );
