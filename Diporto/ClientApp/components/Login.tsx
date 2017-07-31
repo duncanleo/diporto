274c81d3e5ebb4d6a590c87c027b7d60594e82de
Jurvis Tan <jurvis.tan@me.com>
2017-07-31 10:59:58 +0800

    handle login errors

--- Diporto/ClientApp/components/Login.tsx
+++ Diporto/ClientApp/components/Login.tsx
@@ -5,8 +5,9 @@ import { connect } from 'react-redux';
 import { ApplicationState } from '../store';
 import * as AuthState from '../store/Auth';
 
-type LoginProps = 
-  typeof AuthState.actionCreators
+type LoginProps =
+  AuthState.AuthState
+  & typeof AuthState.actionCreators
   & RouteComponentProps<{}>
 
 class Login extends React.Component<LoginProps, {}> {
@@ -22,7 +23,16 @@ class Login extends React.Component<LoginProps, {}> {
 
   render() {
     return (
-      <div>
+      <div className="w-80 self-center pt3">
+        {this.props.errorMessage &&
+          <div className="flex items-center justify-center pa4 bg-washed-red dark-red br3">
+            <svg className="w1" data-icon="info" viewBox="0 0 32 32" style={{fill:"currentcolor"}}>
+              <title>info icon</title>
+              <path d="M16 0 A16 16 0 0 1 16 32 A16 16 0 0 1 16 0 M19 15 L13 15 L13 26 L19 26 z M16 6 A3 3 0 0 0 16 12 A3 3 0 0 0 16 6"></path>
+            </svg>
+            <span className="lh-title ml3">{this.props.errorMessage}</span>
+          </div>
+        }
         <LoginForm
           onLoginPressed={(creds) => this.loginUser(creds)}
         />
@@ -33,6 +43,6 @@ class Login extends React.Component<LoginProps, {}> {
 
 
 export default connect(
-  null,
- AuthState.actionCreators
+  (state: ApplicationState) => state.auth,
+  AuthState.actionCreators
 )(Login) as typeof Login;
