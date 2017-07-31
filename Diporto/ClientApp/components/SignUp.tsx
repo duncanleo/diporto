e50d8244d0ad827fdcc0849265cf15c1663ad784
Jurvis Tan <jurvis.tan@me.com>
2017-07-31 11:15:08 +0800

    flash signup errors on form

--- Diporto/ClientApp/components/SignUp.tsx
+++ Diporto/ClientApp/components/SignUp.tsx
@@ -5,9 +5,10 @@ import { connect } from 'react-redux';
 import { ApplicationState } from '../store';
 import * as AuthState from '../store/Auth';
 
-type LoginProps = 
-  typeof AuthState.actionCreators
-    & RouteComponentProps<{}>
+type LoginProps =
+  AuthState.AuthState
+  & typeof AuthState.actionCreators
+  & RouteComponentProps<{}>
 
 class SignUp extends React.Component<LoginProps, {}> {
   constructor(props) {
@@ -23,7 +24,16 @@ class SignUp extends React.Component<LoginProps, {}> {
 
   render() {
     return (
-      <div>
+      <div className="w-80 self-center pt3">
+        {this.props.errorMessage &&
+          <div className="flex items-center justify-center pa4 bg-washed-red dark-red br3">
+            <svg className="w1" data-icon="info" viewBox="0 0 32 32" style={{fill:"currentcolor"}}>
+              <title>info icon</title>
+              <path d="M16 0 A16 16 0 0 1 16 32 A16 16 0 0 1 16 0 M19 15 L13 15 L13 26 L19 26 z M16 6 A3 3 0 0 0 16 12 A3 3 0 0 0 16 6"></path>
+            </svg>
+            <span className="lh-title ml3">{this.props.errorMessage}</span>
+          </div>
+        }
         <SignupForm
           onSignupPressed={(regCreds) => this.signupUser(regCreds)}
         />
@@ -34,6 +44,6 @@ class SignUp extends React.Component<LoginProps, {}> {
 
 
 export default connect(
-  null,
+  (state: ApplicationState) => state.auth,
  AuthState.actionCreators
 )(SignUp) as typeof SignUp;
